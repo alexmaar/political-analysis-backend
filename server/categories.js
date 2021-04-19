60bb77dab3eb9a385cd1cbe1af03d223f7f67d4e
Simon1PL <saicxs123@gmail.com>
2021-04-20 00:59:30 +0200

    make wueries in endpoint categories async

--- server/categories.js
+++ server/categories.js
@@ -1,26 +1,27 @@
-module.exports = (app, db) => {
-  app.get('/categories', (req, result) => {
-    let political_parties = [];
-    let categories = [];
-    let supporting_strike_options = [];
-    db.all("SELECT DISTINCT supporting_strike FROM users", [], (err, rows) => {
-      if (err) {
-        throw err;
-      }
-      db.all("SELECT DISTINCT category FROM users", [], (err, rows) => {
-        if (err) {
-          throw err;
+function dbAllAsPromise(query, db) {
+  return new Promise(function(resolve, reject) {
+    db.all(query, [], function(err, rows)  {
+        if(err) reject("Read error: " + err.message)
+        else {
+            resolve(rows)
         }
-        db.all(" SELECT DISTINCT political_party FROM users", [], (err, rows) => {
-          if (err) {
-            throw err;
-          }
-          political_parties = rows.map(row => row.political_party);
-          result.send({ categories, political_parties, supporting_strike_options });
-        });
-        categories = rows.map(row => row.category);
-      });
-      supporting_strike_options = rows.map(row => row.supporting_strike);
-    });
+    })
+  });
+}
+
+module.exports = (app, db) => {
+  app.get('/categories', async (req, result) => {
+    try{
+      const political_parties_promise = dbAllAsPromise("SELECT DISTINCT political_party FROM users", db);
+      const categories_promise = dbAllAsPromise("SELECT DISTINCT category FROM users", db);
+      const supporting_strike_options_promise = dbAllAsPromise("SELECT DISTINCT supporting_strike FROM users", db);
+      let [political_parties, categories, supporting_strike_options] = await Promise.all([political_parties_promise, categories_promise, supporting_strike_options_promise]);
+      political_parties = political_parties.map(row => row.political_party);
+      categories = categories.map(row => row.category);
+      supporting_strike_options = supporting_strike_options.map(row => row.supporting_strike);
+
+      result.send({ categories, political_parties, supporting_strike_options });
+    }
+    catch (error) { throw error; }
   });
 };
