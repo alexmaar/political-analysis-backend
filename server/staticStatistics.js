418242ba6021ef49c46b178e6ec9f3cb63ed596a
alexmaar <oo.marzec@gmail.com>
2021-05-01 17:46:01 +0200

    Handle date range.

--- server/staticStatistics.js
+++ server/staticStatistics.js
@@ -15,15 +15,68 @@ function dbAllAsPromise(query, db) {
   
       try{
         let categories = req.body.categories;
+        let dateRange = req.body.dateRange;
         promises = [];
         categories.forEach(element => {
           let supportingStike = 2;
           if(element == 'za')supportingStike = 1;
           if(element == 'przeciw')supportingStike = 0;
-          promises.push(dbAllAsPromise("SELECT COUNT(*) FROM tweets t join users u on u.id = t.user_id WHERE category='" + element + "' OR political_party='" + element + "' OR supporting_strike=" + supportingStike, db));
+          if (dateRange.length === 2){
+            promises.push(dbAllAsPromise("SELECT COUNT(*) FROM tweets t join users u on u.id = t.user_id WHERE (category='" + element + "' OR political_party='" + element + "' OR supporting_strike=" + supportingStike + ") AND t.date BETWEEN date(\"" + dateRange[0].slice(0, 10) + "\") AND date(\"" + dateRange[1].slice(0,10) + "\") ", db));
+          }
+          else{
+            promises.push(dbAllAsPromise("SELECT COUNT(*) FROM tweets t join users u on u.id = t.user_id WHERE (category='" + element + "' OR political_party='" + element + "' OR supporting_strike=" + supportingStike + ")", db));
+          }
         });
         let data = await Promise.all(promises);
         result.send(data.map((e) => e[0]['COUNT(*)']));
+      }
+      catch (error) { throw error; }
+    });
+
+    app.post('/total-likes', async (req, result) => {
+  
+      try{
+        let categories = req.body.categories;
+        let dateRange = req.body.dateRange;
+        promises = [];
+        categories.forEach(element => {
+          let supportingStike = 2;
+          if(element == 'za')supportingStike = 1;
+          if(element == 'przeciw')supportingStike = 0;
+          if (dateRange.length == 2){
+            promises.push(dbAllAsPromise("SELECT SUM(t.likes_count) as likes FROM tweets t join users u on u.id = t.user_id WHERE (category='" + element + "' OR political_party='" + element + "' OR supporting_strike=" + supportingStike + ") AND t.date BETWEEN date(\"" + dateRange[0].slice(0, 10) + "\") AND date(\"" + dateRange[1].slice(0,10) + "\")", db));
+          }
+          else {
+            promises.push(dbAllAsPromise("SELECT SUM(t.likes_count) as likes FROM tweets t join users u on u.id = t.user_id WHERE (category='" + element + "' OR political_party='" + element + "' OR supporting_strike=" + supportingStike + ")", db));
+          }
+        });
+        
+        let data = await Promise.all(promises);
+        result.send(data.map((e) => e[0].likes));
+      }
+      catch (error) { throw error; }
+    });
+
+    app.post('/total-retweets', async (req, result) => {
+  
+      try{
+        let categories = req.body.categories;
+        let dateRange = req.body.dateRange;
+        promises = [];
+        categories.forEach(element => {
+          let supportingStike = 2;
+          if(element == 'za')supportingStike = 1;
+          if(element == 'przeciw')supportingStike = 0;
+          if (dateRange.length == 2){
+            promises.push(dbAllAsPromise("SELECT SUM(t.retweets_count) as likes FROM tweets t join users u on u.id = t.user_id WHERE (category='" + element + "' OR political_party='" + element + "' OR supporting_strike=" + supportingStike + ") AND t.date BETWEEN date(\"" + dateRange[0].slice(0, 10) + "\") AND date(\"" + dateRange[1].slice(0,10) + "\")", db));
+          }
+          else {
+            promises.push(dbAllAsPromise("SELECT SUM(t.retweets_count) as likes FROM tweets t join users u on u.id = t.user_id WHERE (category='" + element + "' OR political_party='" + element + "' OR supporting_strike=" + supportingStike + ")", db));
+          }
+        });
+        let data = await Promise.all(promises);
+        result.send(data.map((e) => e[0].likes));
       }
       catch (error) { throw error; }
     });
